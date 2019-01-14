a34c50339ab090ae2ec253ffa2263e9a92f39759
shirodoni <jishoindoa@gmail.com>
2019-01-14 17:16:45 +0900

    add 2D表示 とりあえず2Dでシミュレーションの状態を確認できるようにした。

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
+const screenW = 600;
+const screenH = 600;
+const waveW = 100;
+const waveH = 100;
+
+
 class Main {
     init() {
+        //試験的に2Dのcanvasを利用
+        this.canvas = document.getElementById("mycanvas");
+        this.canvas.width = screenW;
+        this.canvas.height = screenH;
+        this.context = this.canvas.getContext("2d");
+
+
+        this.wave = new WaveField();
+        this.wave.init(waveW, waveH, 1.0, 30.0);
+
+        this.wave.makeWave(50, 50, 1.0, 6.0);
     }
     update() {
+        this.wave.step(1 / 60);
     }
     draw() {
+        let ctx = this.context;
+        ctx.fillStyle = "rgb(0,0,0)"
+        ctx.fillRect(0, 0, screenW, screenH);
+
+        for (let i = 0; i < waveW; i++) {
+            for (let j = 0; j < waveH; j++) {
+                //ctx.fillStyle = "hsl(" + (this.wave.get_uCur(i, j) * 500) + ", 50%, 50%)";
+                ctx.fillStyle = "hsl(0, 0%, " + (50 + this.wave.get_uCur(i, j) * 40) + "%)";
+                ctx.fillRect(i * screenW / waveW, j * screenH / waveH, screenW / waveW, screenH / waveH);
+            }
+            // ctx.fillStyle = "rgb(255,255,255)";
+            // ctx.fillRect(i * screenW / waveW, 0, screenW / waveW, screenH / 2 - 100 * this.wave.get_uCur(i, 50));
+        }
     }
     main() {
         const loop = () => {
@@ -15,8 +46,8 @@ class Main {
     }
 }
 
+const main = new Main();
 window.addEventListener("load", () => {
-    const main = new Main();
     main.init();
     main.main();
 });
