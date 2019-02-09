c481c55668cd73987e8d19d1b55e5a0a54dfb87b
shirodoni <jishoindoa@gmail.com>
2019-02-09 13:37:08 +0900

    add waveRenderer,click マウスクリック位置に波が立つように

--- main.js
+++ main.js
@@ -6,14 +6,23 @@ const waveH = 100;
 
 class Main {
     init() {
+        this.canvas = document.querySelector("#myCanvas");
+
         this.wave = new WaveField();
-        this.wave.init(waveW, waveH, 1.0, 30.0, 0.1);
-        this.wave.makeWave(20, 30, 1.0, 6.0);
+        this.wave.init(waveW, waveH, 1.0, 30.0, 0.2);
+        //this.wave.makeWave(20, 30, 1.0, 6.0);
 
         this.waveRenderer = new WaveRenderer();
-        this.waveRenderer.init(this.wave);
+        this.waveRenderer.init(this.wave, this.canvas);
+
+        this.mouseInput = new MouseInput();
+        this.mouseInput.init(this.canvas);
     }
     update() {
+        this.mouseInput.update();
+        if (this.mouseInput.down && !this.mouseInput.downOld) {
+            this.waveRenderer.click(-1 + this.mouseInput.x / screenW * 2, 1 - this.mouseInput.y / screenH * 2);
+        }
         this.wave.step(1 / 60);
     }
     draw() {
